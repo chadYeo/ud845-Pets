8775b9358cf87bd14b781c86a5c96e2334e0e025
Chad Yeo <chad.yeo.cy@gmail.com>
2017-02-24 14:41:27 -0500

    Added ContentProvider

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -1,7 +1,47 @@
 package com.example.android.pets.data;
 
-/**
- * Created by chad.yeo on 2/24/2017.
- */
-public class PetProvider {
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+public class PetProvider extends ContentProvider {
+
+    public static final String LOG_TAG = PetProvider.class.getSimpleName();
+
+    @Override
+    public boolean onCreate() {
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
+        return 0;
+    }
 }
